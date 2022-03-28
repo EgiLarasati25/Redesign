48ca1f41707b658a25b182e8bc3c2a6434c3353f
EgiLarasati25 <12050123653@students.uin-suska.ac.id>
2022-03-28 14:35:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dana/danaactivity/loading.java
@@ -0,0 +1,44 @@
+package com.example.dana.danaactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dana.R;
+
+public class loading extends AppCompatActivity {
+    protected void onCreate (Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+
+        setContentView(R.layout.loading);
+    }
+    public void clickloading(View view) {
+        Intent i = new Intent(this, login.class);
+        startActivity(i);
+    }
+    public void clicklogin(View view) {
+        Intent i = new Intent(this, home.class);
+        startActivity(i);
+    }
+    public void clickhome(View view) {
+        Intent i = new Intent(this, riwayat.class);
+        startActivity(i);
+    }
+
+    public void clickriwayat(View view) {
+        Intent i = new Intent(this, pocket.class);
+        startActivity(i);
+    }
+
+    public void clickpocket(View view) {
+        Intent i = new Intent(this, akun.class);
+        startActivity(i);
+    }
+
+    public void clickakun(View view) {
+        Intent i = new Intent(this, akun.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/com/example/dana/danaactivity/pocket.java
@@ -0,0 +1,44 @@
+package com.example.dana.danaactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dana.R;
+
+public class pocket extends AppCompatActivity{
+
+        protected void onCreate (Bundle saveInstanceState) {
+            super.onCreate(saveInstanceState);
+
+            setContentView(R.layout.pocket);
+        }
+        public void clickloading(View view) {
+            Intent i = new Intent(this, loading.class);
+            startActivity(i);
+        }
+        public void clicklogin(View view) {
+            Intent i = new Intent(this, login.class);
+            startActivity(i);
+        }
+        public void clickhome(View view) {
+            Intent i = new Intent(this, home.class);
+            startActivity(i);
+        }
+        public void clickriwayat(View view) {
+            Intent i = new Intent(this, riwayat.class);
+            startActivity(i);
+        }
+        public void clickpocket(View view) {
+            Intent i = new Intent(this, pocket.class);
+            startActivity(i);
+        }
+        public void clickakun(View view) {
+            Intent i = new Intent(this, akun.class);
+            startActivity(i);
+        }
+
+    }
+
